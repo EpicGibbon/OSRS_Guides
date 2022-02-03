9320293fda4d34886518f92ec380c18cfcb47d75
EpicGibbon <michant1997@gmail.com>
2022-02-03 18:48:04 -0500

    added pathing for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {Link} from "react-router-dom";
+import Nav from './components/Nav'
 
 function App() {
   return (
+    
     <div className="App">
+
       <header className="App-header">
         <p>
           OSRS Guides
+          <img src="http://thegg.net/wp-content/uploads/2021/02/these-are-some-top-old-school-runescape-bosses-to-fight-in-2021-header.jpg" />
         </p>
         <a
           className="App-link"
