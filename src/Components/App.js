442767a733936c367e6388ffcf18a16c40535a32
ryanschuttig <ryanschuttig@gmail.com>
2022-02-04 20:01:01 -0500

    basic file setup

---
+++ src/Components/App.js
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+
+function Main() {
+  return (
+    <header>OSRS Guides
+      <img src="http://thegg.net/wp-content/uploads/2021/02/these-are-some-top-old-school-runescape-bosses-to-fight-in-2021-header.jpg"></img>
+    </header>
+  )
+}
+
+// function App() {
+//   return (
+    
+//     <div className="App">
+
+//       <header className="App-header">
+//         <p>
+//           OSRS Guides
+//           <img src="http://thegg.net/wp-content/uploads/2021/02/these-are-some-top-old-school-runescape-bosses-to-fight-in-2021-header.jpg" />
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default Main;
---
+++ src/Pages/Main/index.js
@@ -0,0 +1,14 @@
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
+import '../Main/style.css';
+
+export const Main = () => {
+
+    return (
+        <header>OSRS Guides
+            <img src="http://thegg.net/wp-content/uploads/2021/02/these-are-some-top-old-school-runescape-bosses-to-fight-in-2021-header.jpg"></img>
+        </header>
+    )
+}
+
+export default Main;
