270b5ece11ef2d454925d3c9e29b24981f0e0b62
EpicGibbon <michant1997@gmail.com>
2022-02-06 20:32:26 -0500

    created basic footer and boss card components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "../Pages/Main";
-import Header from "../Components/Header";
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../Pages/Main';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import Bosses from '../Components/Bosses';
 
 function App() {
-  return (
-    <>
-    <Header />
-    <Main />
-    </>
-  )
-};
+	return (
+		<>
+			<Header />
+      {/* only have boss cards at this level to make sure it renders; still having issues getting react router configured.  */}
+      <Bosses />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={Main} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</>
+	);
+}
 
-
-export default App
+export default App;
---
+++ src/Components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css"
+
+function Footer(){
+    return (
+        <footer className="footer">
+            <h4> Made by the dreamteam</h4>
+            
+        </footer>
+    )
+}
+
+export default Footer;
